a578904061c4e223cdc0b4c96761bc5565c5b7be
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-01-31 17:05:23 -0500

    Done.

--- src/init.js
+++ src/init.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   const ctx = document.querySelector("#NYCWeatherChart").getContext("2d")
-  const API_KEY = "e493e6788dbe973871b0e4344527bdd7";
+  const API_KEY = "i deleted the key";
   const CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
   const DARK_SKY_BASE = "https://api.darksky.net/"
   const URL = `${CORS_WRAPPER}${DARK_SKY_BASE}/forecast/${API_KEY}/40.7127,-74.0059?exclude=currently,minutely,daily`
