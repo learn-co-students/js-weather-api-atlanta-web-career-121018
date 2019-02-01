ffa6f20527dff2f32f84121588f58703ffd3c938
github username <github email address>
2019-02-01 15:05:39 -0500

    Done.

--- src/weatherChart.js
+++ src/weatherChart.js
@@ -1,9 +1,23 @@
+
+
+
 function formatHours(hourlyData){
-  // your code here
-}
+    // console.log(hourlyData.data)
+    let newArray = [];
+    hourlyData.data.forEach(function(data){
+            let time = (new Date(data.time * 1000).getHours())
+            newArray.push(time)
+    })
+    return newArray
+};
 
 function formatFahrenheit(hourlyData){
-  // your code here
+    let newArray = [];
+    hourlyData.data.forEach(function(data){
+            let temp = data.temperature
+            newArray.push(temp)
+    })
+    return newArray
 }
 
 function generateDataSet(hours, temperatures) {
@@ -26,7 +40,16 @@ function generateDataSet(hours, temperatures) {
   }
 }
 
+
 function makeRequest(endpoint, canvas) {
+    fetch(endpoint)
+    .then(res => res.json())
+    .then(res => {
+        let hourlyData = res.hourly
+        new Chart(canvas, generateDataSet(formatHours(hourlyData), formatFahrenheit(hourlyData)))
+    })
+};
+
   // Your code goes here
   // After your fetch works - format the response using the helper functions above:
   // convert darksky timestamps
@@ -37,4 +60,3 @@ function makeRequest(endpoint, canvas) {
   // const chartDataset = generateDataSet(formattedHours, formattedTemps)
   // append the chart to the DOM
   // new Chart(canvas, chartDataset)
-}
